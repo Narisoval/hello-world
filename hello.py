34a647ccded6bf3feab6bcd6ca2f70f36a28df8c
Narisoval <bod13773@gmail.com>
2020-08-20 16:57:51 +0300

    updated hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,6 @@
-a = []
+pyramid = ''
 for i in range(10):
-    for k in range(i,-1,-1):
-        a.append(str(k))
-    a.append('\n')
-pyr  = ''
-for i in a:
-    pyr += i
-print(pyr)
+    for k in range(i, -1, -1):
+        pyramid += str(k)
+    pyramid += ('\n')
+print(pyramid)
